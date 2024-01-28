fd88326e91d2219ecdb517d87e45814337c99895
LuisYerga <lyergamayor1@gmail.com>
2024-01-28 18:18:28 +0100

    Intento controller auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    
+    public function login(Request $request)
+	{
+	    // Comprobamos que el email y la contraseña han sido introducidos
+	    $request->validate([
+	        'email' => 'required',
+	        'password' => 'required',
+	    ]);
+	
+	    // Almacenamos las credenciales de email y contraseña
+	    $credentials = $request->only('email', 'password');
+	
+	    // Si el usuario existe lo logamos y lo llevamos a la vista de "logados" con un mensaje
+	    if (Auth::attempt($credentials)) {
+	        return redirect()->intended('index')
+	            ->withSuccess('Logado Correctamente');
+	    }
+	
+	    // Si el usuario no existe devolvemos al usuario al formulario de login con un mensaje de error
+	    return redirect("/login")->withSuccess('Los datos introducidos no son correctos');
+	}
+	
 }
