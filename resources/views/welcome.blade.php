31e13dbd78b35c41b5462640a94521e7b0b0613d
Marcos <Marcoscabet@gmail.com>
2024-02-07 22:50:18 +0100

    carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,45 @@
 @extends('layouts.plantilla')
 
 @section('title', "Inicio")
+
+@section('content')
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="/img/almacen-dia.png" class="d-block w-100" alt="imagen1-carousel">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>TEXTO</h5>
+          <p>TEXTO</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="/img/fabrica-noche.png" class="d-block w-100" alt="imagen2-carousel">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>TEXTO</h5>
+          <p>TEXTO</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="/img/oficina-dia.png" class="d-block w-100" alt="imagen3-carousel">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>TEXTO</h5>
+          <p>TEXTO</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  
+@endsection
