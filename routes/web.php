ca5512aebdd9041db1c1bdcb81be2508fa21a8bb
Luis Yerga <lyergamayor1@gmail.com>
2024-02-12 14:19:07 +0100

    Me ha ignorado los cambios

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AddressController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\WishlistController;
 
-use App\Http\Controllers\WishlistController;
 use App\Models\Cart;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Redirect;
@@ -40,31 +38,7 @@ Fortify::verifyEmailView(function () {
     return view('auth.verify-email');
 });
 
-Route::get('/', [ProductController::class, 'getProducts']);
-
-
-Route::post('additem', [CartController::class, 'addItem'])->name('cart.additem');
-Route::post('clearcart', [CartController::class, 'clearCart'])->name('cart.clearcart');
-Route::post('removeitem', [CartController::class, 'removeItem'])->name('cart.removeitem');
-Route::post('/cart/purchase', [CartController::class, 'purchase'])->name('cart.purchase');
-
-Route::post('createProduct', [ProductController::class, 'create'])->name('layouts.createProduct');
-Route::put('edit_product/{id}', [ProductController::class, 'update'])->name('layouts.updateProduct');
-Route::delete('delete_product/{id}', [ProductController::class, 'delete'])->name('layouts.deleteProduct');
-
-Route::post('createProduct', [ProductController::class, 'create'])->name('layouts.createProduct');
-Route::put('edit_product/{id}', [ProductController::class, 'update'])->name('layouts.updateProduct');
-Route::delete('delete_product/{id}', [ProductController::class, 'delete'])->name('layouts.deleteProduct');
-
-Route::get('/my-orders', [OrderController::class, 'index'])->name('orders.index')->middleware('auth');
-Route::get('/addresses', [AddressController::class, 'index'])->name('addresses.index');
-Route::get('/account/edit', [AccountController::class, 'edit'])->name('account.edit');
-
-
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::post('updateAmount', [CartController::class, 'updateItemAmount'])->name('cart.updateAmount');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::middleware('admin')->group(function(){
     Route::get('adminProduct', [ProductController::class, 'adminIndex'])->name('admin.index');
@@ -75,7 +49,6 @@ Route::middleware('admin')->group(function(){
 
 Route::get('/', [ProductController::class, 'getProducts']); //Mostrar productos
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::middleware('auth', 'verified')->group(function(){
     Route::post('additem', [CartController::class, 'addItem'])->name('cart.additem');
     Route::post('clearcart', [CartController::class, 'clearCart'])->name('cart.clearcart');
