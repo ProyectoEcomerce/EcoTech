8aa336b7b36ddca6d4316d330d9ecc9d9f905512
LuisYerga <lyergamayor1@gmail.com>
2024-02-13 18:27:51 +0100

    Añado transacciones a los controladores

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class AccountController extends Controller
@@ -15,34 +16,50 @@ class AccountController extends Controller
 
     public function updatePassword(Request $request)
     {
-        $request->validate([
-            'current_password' => 'required',
-            'new_password' => 'required|string|min:8|confirmed',
-        ]);
-
-        $user = Auth::user();
-
-        if (!Hash::check($request->current_password, $user->password)) {
-            return back()->withErrors(['current_password' => 'La contraseña actual no es correcta.']);
+        DB::beginTransaction();
+        try{
+            $request->validate([
+                'current_password' => 'required',
+                'new_password' => 'required|string|min:8|confirmed',
+            ]);
+    
+            $user = Auth::user();
+    
+            if (!Hash::check($request->current_password, $user->password)) {
+                return back()->withErrors(['current_password' => 'La contraseña actual no es correcta.']);
+            }
+    
+            $user->password = Hash::make($request->new_password);
+            $user->save();
+    
+            DB::commit();
+            return view('account.edit')->with('success', 'Contraseña actualizada con éxito.');
+        }catch(\Exception $e){
+            DB::rollBack();
+            return back()->withErrors('No se pudo actualizar la contraseña');
         }
-
-        $user->password = Hash::make($request->new_password);
-        $user->save();
-
-        return view('account.edit')->with('success', 'Contraseña actualizada con éxito.');
+        
     }
 
     public function changeLocal(Request $request){
 
         if(Auth::check()){
-            $user=Auth::user();
-            $language=$request->route('locale');
-            $user->language=$language;
-            $user->save();
+            DB::beginTransaction();
+            try{
+                $user=Auth::user();
+                $language=$request->route('locale');
+                $user->language=$language;
+                $user->save();
+                DB::commit();
+                return redirect()->back()->with('success', 'Idioma cambiado.');
+            }catch(\Exception $e){
+                DB::rollBack();
+                return back()->withErrors('No se pudo actualizar el idioma');
+            }
         }else{
             $language = $request->route('locale');
             session()->put('locale', $language);
+            return redirect()->back()->with('success', 'Idioma cambiado.');
         }
-        return redirect()->back()->with('success', 'Idioma cambiado.');
     }
 }
--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -20,13 +21,19 @@ class OrderController extends Controller
         // Asegúrate de que el usuario autenticado sea el propietario del pedido
         if ($order->user_id !== Auth::id()) {
             abort(403, 'No estás autorizado para realizar esta acción.');
+        }else{
+            DB::beginTransaction();
+            try{
+                // Cambiar el estado del pedido a "cancelado"
+                $order->status = 'cancelado';
+                $order->save();
+                DB::commit();
+                // Redireccionar al usuario con un mensaje de éxito
+                return back()->with('success', 'El pedido ha sido cancelado con éxito.');
+            }catch(\Exception $e){
+                DB::rollBack();
+                return back()->withErrors('No se pudo cancelar el pedido');
+            }   
         }
-
-        // Cambiar el estado del pedido a "cancelado"
-        $order->status = 'cancelado';
-        $order->save();
-
-        // Redireccionar al usuario con un mensaje de éxito
-        return back()->with('success', 'El pedido ha sido cancelado con éxito.');
     }
 }
