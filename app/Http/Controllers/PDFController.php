9e9134f0ffbb0db8f544530f073a179bbd34c012
Manuel Soto <manuelsr0113@gmail.com>
2024-02-13 14:24:05 +0100

    PASO 7 DE LA GUIA PARA IMPLEMENTAR PDF

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Barryvdh\DomPDF\PDF as DomPDFPDF;
+use PDF;
+
+use Illuminate\Http\Request;
+
+class PDFController extends Controller
+{
+    public function generatePDF()
+    {
+        $data = ['title' => 'domPDF in Laravel 10'];
+        $pdf = PDF::loadView('pdf.document', $data);
+        return $pdf->download('document.pdf');
+    }
+}
